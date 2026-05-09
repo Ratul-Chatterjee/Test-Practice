c2ddb11db8eef74ba330860120823a1f18215498
Ratul Chatterjee <174613333+Ratul-Chatterjee@users.noreply.github.com>
2026-05-09 09:52:56 +0530

    Refactor heart attack risk model predictor code

--- A3(1).py
+++ A3(1).py
@@ -1,4 +1,4 @@
-#heart attack risk model predictor
+# heart attack risk model predictor
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
